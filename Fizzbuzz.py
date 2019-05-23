ef58047d28f5742d21ce896a2c5bfab0941cda4f
cyonemori <chikara.yonemori@tri-ad.global>
2019-05-23 16:37:56 +0900

    fdfd

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,4 +1,4 @@
-def fizz_buzz(max_number: int = 100) -> int:
+def fizz_buzz(max_number=100):
     for i in range(1,max_number+1):
         if i%15==0: # 15 is GCM of 3 and 5
             print("Fizz Buzz")
@@ -8,5 +8,3 @@ def fizz_buzz(max_number: int = 100) -> int:
             print("Buzz")
         else:
             print(i)
-
-
