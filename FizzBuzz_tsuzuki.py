853421bc5a1f481eaad58c4fb29aad885bf13571
ytsuzuki <yuta.tsuzuki@tri-ad.global>
2019-05-23 16:30:19 +0900

    fix tsuzuki's fizzbuzz script

--- FizzBuzz_tsuzuki.py
+++ FizzBuzz_tsuzuki.py
@@ -1,9 +1,9 @@
 for num in range(1,101):
-    if num % 3 == 0:
+    if num % 3 == 0 and num % 5 == 0:
+        print("Fizz Buzz")
+    elif num % 3 == 0:
         print("Fizz")
     elif num % 5 == 0:
         print("Buzz")
-    elif num % 3 == 0 and num % 5 == 0:
-        print("Fizz Buzz")
     else:
         print(num)
