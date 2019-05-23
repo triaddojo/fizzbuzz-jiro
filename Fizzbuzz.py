98df23c89867678e532a16ec8cfae380a6116c37
cyonemori <chikara.yonemori@tri-ad.global>
2019-05-23 16:58:19 +0900

    test

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,10 +1,22 @@
-def fizz_buzz(max_number=100):
-    for i in range(1,max_number+1):
-        if i%15==0: # 15 is GCM of 3 and 5
+#!/usr/bin/python
+
+
+def fizz_buzz(max_number: int = 100) -> int:
+    """
+
+    :type max_number: int
+    """
+    for i in range(1, max_number + 1):
+        if i % 15 == 0:  # 15 is GCM of 3 and 5
             print("Fizz Buzz")
-        elif i%3 == 0:
+        elif i % 3 == 0:
             print("Fizz")
-        elif i%5 == 0:
+        elif i % 5 == 0:
             print("Buzz")
         else:
             print(i)
+
+
+if __name__ == '__main__':
+    fizz_buzz()
+
---
+++ log_challenge.py
@@ -0,0 +1,8 @@
+
+
+def get_unitque_IP(log_data):
+    pass
+
+def access_log_generator(filename):
+    with open(filename, "r") as f:
+        f.readlines()
